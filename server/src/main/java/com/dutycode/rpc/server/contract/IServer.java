2f54df3237c09e96198fe45663afa405fb9d0cd0
losetowin <dutycode@gmail.com>
2017-01-17 21:03:09 +0800

    DEV Server接口定义

---
+++ server/src/main/java/com/dutycode/rpc/server/contract/IServer.java
@@ -0,0 +1,7 @@
+package com.dutycode.rpc.server.contract;
+
+public interface IServer {
+
+	void start() throws Exception;
+	void stop() throws Exception;
+}
---
+++ server/src/main/java/com/dutycode/rpc/server/core/SocketServer.java
@@ -0,0 +1,5 @@
+package com.dutycode.rpc.server.core;
+
+public class SocketServer {
+
+}
